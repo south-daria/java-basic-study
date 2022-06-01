5b119eda684a8128337d74962ec80e0e718e0631
jynam821 <daria020821@gmail.com>
2022-06-02 02:26:39 +0900

    변수 테스트

---
+++ src/firstday/VarTest1.java
@@ -0,0 +1,10 @@
+package firstday;
+
+public class VarTest1 {
+    public static void main(String[] args) {
+        //Java에서 '//'는 주석
+        String message; //문자형 변수 message를 선언
+        message = "Hello World"; //message변수에 "Hello World"를 대입
+        System.out.println(message);
+    }
+}
---
+++ src/firstday/VarTest2.java
@@ -0,0 +1,9 @@
+package firstday;
+
+public class VarTest2 {
+    public static void main(String[] args) {
+        String name = "남정윤";
+        int age = 21;
+        System.out.println("나의 이름은 " + name + "이고, 나이는 " + age + " 입니다.");
+    }
+}
